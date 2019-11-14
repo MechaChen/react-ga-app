3f497ed557fbe49221aa5fbf0871e63e23e10b2e
paradisemrch <tabooyu@gmail.com>
2019-11-14 16:48:35 +0800

    counter

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Product = (props) => {
-
-    return (
-        <div className="product">
-            <h2 className="product__name">{props.name}</h2>
-            <span className="product__index">Product {props.index}</span>
-            <img className="product__image" src={props.src} />
-            <div className="product__buttons__wrapper">
-                <button className="product__button" onClick={props.handlePurchase}>BUY</button>
-            </div>
-        </div>
-    );
-}
+const Product = props => {
+  const [count, setCount] = useState(0);
+  const countMiles = () => {
+    setCount(count - 1);
+  };
+  const countPlus = () => {
+    if (count >= 0) {
+      setCount(count + 1);
+    }
+  };
+  return (
+    <div className="product">
+      <h2 className="product__name">{props.name}</h2>
+      <span className="product__index">Product {props.index}</span>
+      <img className="product__image" src={props.src} />
+      <div className="product__counter">
+        <button className="product__button product__num" onClick={countMiles}>
+          -
+        </button>
+        <div className="num">{count}</div>
+        <button className="product__button product__num" onClick={countPlus}>
+          +
+        </button>
+      </div>
+      <div className="product__buttons__wrapper">
+        <button className="product__button" onClick={props.handlePurchase}>
+          BUY
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import ReactGA from 'react-ga';
-import Product from '@components/Product';
-import Tomb from '@images/tomb.png';
-import Potion from '@images/potion.png';
-import Axe from '@images/axe.png';
-import Ghost from '@images/ghost.png';
-import Eye from '@images/eye.png';
+import React, { useState, useEffect } from "react";
+import ReactGA from "react-ga";
+import Product from "@components/Product";
+import Tomb from "@images/tomb.png";
+import Potion from "@images/potion.png";
+import Axe from "@images/axe.png";
+import Ghost from "@images/ghost.png";
+import Eye from "@images/eye.png";
 
 const Products = () => {
-    const products = [
-        {
-            name: 'tomb',
-            src: Tomb
-        },
-        {
-            name: 'potion',
-            src: Potion
-        },
-        {
-            name: 'axe',
-            src: Axe
-        },
-        {
-            name: 'ghost',
-            src: Ghost
-        },
-        {
-            name: 'eye',
-            src: Eye
-        }
-    ];
-
-    const handlePurchase = (index) => {
-        console.log('ReactGA event fire');
-        ReactGA.event({
-            category: 'Purchase',
-            action: `Buy ${index} product`,
-        });
+  const products = [
+    {
+      name: "tomb",
+      src: Tomb
+    },
+    {
+      name: "potion",
+      src: Potion
+    },
+    {
+      name: "axe",
+      src: Axe
+    },
+    {
+      name: "ghost",
+      src: Ghost
+    },
+    {
+      name: "eye",
+      src: Eye
     }
+  ];
 
-    return (
-        <div className="products">
-            <div className="container">
-                <h1>Products</h1>
-                <div className="products__wrapper">
-                    {products.map((prod, i) => (
-                        <Product 
-                            key={i} 
-                            name={prod.name} 
-                            index={i} 
-                            src={prod.src} 
-                            handlePurchase={() => handlePurchase(i)}
-                        />
-                    ))}
-                </div>
-            </div>
+  const handlePurchase = i => {
+    console.log("ReactGA event fire");
+    ReactGA.event({
+      category: "Purchase",
+      action: `Buy ${i} product`
+    });
+  };
+
+  return (
+    <div className="products">
+      <div className="container">
+        <h1>Products</h1>
+        <div className="products__wrapper">
+          {products.map((prod, i) => (
+            <Product
+              key={i}
+              name={prod.name}
+              index={i}
+              src={prod.src}
+              handlePurchase={() => handlePurchase(i)}
+            />
+          ))}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Products;
